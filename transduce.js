47378a6ecd98f614d4eda92aa866301d93da9f84
Kevin Beaty <kevin@simplectic.com>
2014-10-17 21:41:41 -0500

    Initial implementation

--- transduce.js
+++ transduce.js
@@ -1,1 +1,17 @@
+exportLibs(['protocol', 'array', 'push', 'math']);
 
+function exportLibs(libs){
+  var i=0, len = libs.length;
+  for(; i < len; i++){
+    exportLib(libs[i]);
+  }
+}
+
+function exportLib(lib){
+  var library = require('transduce-'+lib), key;
+  for(key in library){
+    if(library.hasOwnProperty(key)){
+      module.exports[key] = library[key];
+    }
+  }
+}
